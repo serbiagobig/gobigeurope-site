696ca8cdcd279281f46f60f682be27a167599700
serbiagobig <serbiagobig@gmail.com>
2026-09-06 20:52:19 +0300

    Restore shared Projects hub in navigation

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ cp projects.html dist/projects.html
 cp berry-harvesting.html dist/berry-harvesting.html
 python scripts/final_site_fixes.py
 python scripts/compact_project_cards.py
+python scripts/restore_projects_hub.py
