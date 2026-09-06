ecb42272b3507bebfe5256f6f3481ae4ed886c85
serbiagobig <serbiagobig@gmail.com>
2026-09-06 21:06:28 +0300

    Always publish blog page in main build

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ set -eu
 sh build-base.sh
 cp projects.html dist/projects.html
 cp berry-harvesting.html dist/berry-harvesting.html
+cp blog.html dist/blog.html
+if [ -f assets/blog-data.json ]; then
+  cp assets/blog-data.json dist/assets/blog-data.json
+fi
 python scripts/final_site_fixes.py
 python scripts/compact_project_cards.py
 python scripts/restore_projects_hub.py
